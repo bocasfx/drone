03106c50c92df2a0cd33d52b5cc1f77c97aa96a6
Rodolfo Palacios <rpalacios@igloosoftware.com>
2016-04-05 16:16:01 -0400

    Drag and drop-ish

--- components/Looper.jsx
+++ components/Looper.jsx
@@ -1,23 +1,55 @@
 'use strict';
 
-const React = require('react');
-const ReactDOM = require('react-dom');
+const React       = require('react');
+const ReactDOM    = require('react-dom');
 const ProgressBar = require('progressbar.js');
+const dragSource  = require('react-dnd').DragSource;
+const PropTypes   = React.PropTypes;
+
+const looperSource = {
+  beginDrag: function (props) {
+    return props;
+  },
+
+  endDrag: function(props, monitor, component) {
+    component.setPosition(500, 500);
+  }
+}
+
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  };
+}
 
 const Looper = React.createClass({
+  propTypes: {
+    xPos: PropTypes.string.isRequired,
+    yPos: PropTypes.string.isRequired,
+    // Injected by React DnD:
+    isDragging: PropTypes.bool.isRequired,
+    connectDragSource: PropTypes.func.isRequired
+  },
+
   getInitialState: function() {
     return {
       isPlaying: false,
       loop: true,
       progress: 0,
       audioNode: null,
-      progressBar: null
+      progressBar: null,
+      xPos: 0,
+      yPos: 0
     };
   },
 
   componentDidMount: function() {
     let domNode = ReactDOM.findDOMNode(this)
     let audioNode = domNode.children[0];
+
+    this.state.xPos = this.props.xPos;
+    this.state.yPos = this.props.yPos;
 
     audioNode.addEventListener('timeupdate', this.updateProgress, false);
     this.state.audioNode = audioNode;
@@ -31,6 +63,7 @@ const Looper = React.createClass({
       trailWidth: 1,
       trailColor: '#999'
     });
+    this.forceUpdate();
   },
 
   handleClick: function() {
@@ -48,6 +81,11 @@ const Looper = React.createClass({
     }
   },
 
+  setPosition: function(x, y) {
+    this.state.xPos = x;
+    this.state.yPos = y;
+  },
+
   updateProgress: function() {
     let audioNode = this.state.audioNode;
     let value = 0;
@@ -60,8 +98,19 @@ const Looper = React.createClass({
   },
 
   render: function() {
-    return (
-      <div>
+    let isDragging = this.props.isDragging;
+    let connectDragSource = this.props.connectDragSource;
+    let xPos = this.state.xPos;
+    let yPos = this.state.yPos;
+    let opacity = isDragging ? 0.5 : 1;
+    let style = {
+      opacity: opacity,
+      left: xPos + 'px',
+      top: yPos + 'px'
+    };
+
+    return connectDragSource(
+      <div className="looper" style={style}>
         <audio src={this.props.src}>
           <div>Sorry :(</div>
         </audio>
@@ -71,4 +120,5 @@ const Looper = React.createClass({
   }
 })
 
-module.exports = Looper;
+// module.exports = Looper;
+module.exports = dragSource('looper', looperSource, collect)(Looper);
--- components/Mixer.jsx
+++ components/Mixer.jsx
@@ -1,17 +1,19 @@
 'use strict';
 
-const React   = require('react');
-const Looper  = require('./Looper.jsx');
+const React           = require('react');
+const Looper          = require('./Looper.jsx');
+const HTML5Backend    = require('react-dnd-html5-backend');
+const dragDropContext = require('react-dnd').DragDropContext;
 
 const Mixer = React.createClass({
   render: function() {
     return (
       <div>
-        <Looper src="media/loop.mp3"/>
-        <Looper src="media/loop2.mp3"/>
+        <Looper src="media/loop.mp3" xPos="100" yPos="100"/>
+        <Looper src="media/loop2.mp3" xPos="200" yPos="200"/>
       </div>
     );
   }
 });
 
-module.exports = Mixer;
+module.exports = dragDropContext(HTML5Backend)(Mixer);
